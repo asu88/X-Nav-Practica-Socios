70dd3520d02ffc9c3890fb7c0a1a2b9ef0b880a2
asu88 <asu.ramon88@gmail.com>
2015-03-19 23:26:36 +0100

    sirviendo json sin el update

--- js/api.js
+++ js/api.js
@@ -1,6 +1,9 @@
 
 jQuery(document).ready(function() {
- 
+    
+  //  function myline() {
+  //  }
+
     $( "#date" ).datepicker(); // Desplegar el calendario al hacer click sobre el input
 
     $( "#mySliderDiv" ).slider({  // Un slider horizontal, se puede usar para formularios 
@@ -11,15 +14,35 @@ jQuery(document).ready(function() {
     });
 
     $( "#menu" ).menu();
-  //  $("div").draggable() ;     // Arrastrar cualquier div
-  //  $( "div" ).resizable();   // Maximizar y minimizar el div arrastrando
-
+  
     $( "#selectable" ).selectable();  //Selecccionar un solo item en una lista
      $( "#accordion" ).accordion();   // clikc y desplegar texto
-     $("#tabs-1").accordion();
-     $("#tabs-2").accordion();
-     $("#tabs-3").accordion();
+   //  $("#tabs-1").accordion();
+    // $("#tabs-2").accordion();
+    // $("#tabs-3").accordion();
      
      $( "#tabs" ).tabs(); // Tabla con distintos textos
+
+    
+    /*Mensajes del usuario*/
+       $.getJSON("/json/myline.json", function(data) {
+      //console.log(data.myline[0].autor);
+      for(var i=0; i<data.myline.length; i++) {   
+            $('#tabs-1').prepend('<h3>' + data.myline[i].autor + ": "+data.myline[i].titulo +'</h3>'+'<div><p>'+data.myline[i].contenido+'</p></div>');    
+	  } ;
+      $("#tabs-1").accordion(); 
+	});
+
+    /*Mensajes de los socios */
+
+      $.getJSON("/json/timeline.json", function(data) {
+     // console.log(data.myline[0].autor);
+      for(var i=0; i<data.timeline.length; i++) {   
+           console.log(data.timeline[i].autor);           
+         $('#tabs-2').prepend('<h3>' + data.timeline[i].autor + ": "+data.timeline[i].titulo +'</h3>'+'<div><p>'+data.timeline[i].contenido+'</p></div>');    
+	  } ;
+      $("#tabs-2").accordion(); 
+	});
+
 
 });
