cb25a3c441780c5c94446d98cf20df0ffa533b80
asu88 <asu.ramon88@gmail.com>
2015-03-19 02:40:01 +0100

    Interfaz versin 1.0

---
+++ api.js
@@ -0,0 +1,21 @@
+
+jQuery(document).ready(function() {
+ 
+    $( "#date" ).datepicker(); // Desplegar el calendario al hacer click sobre el input
+
+    $( "#mySliderDiv" ).slider({  // Un slider horizontal, se puede usar para formularios 
+        orientation: "horizontal",
+         min: 0,
+        max: 80,
+        value: 50
+    });
+
+    $( "#menu" ).menu();
+  //  $("div").draggable() ;     // Arrastrar cualquier div
+  //  $( "div" ).resizable();   // Maximizar y minimizar el div arrastrando
+
+    $( "#selectable" ).selectable();  //Selecccionar un solo item en una lista
+     $( "#accordion" ).accordion();   // clikc y desplegar texto
+     $( "#tabs" ).tabs(); // Tabla con distintos textos
+
+});
